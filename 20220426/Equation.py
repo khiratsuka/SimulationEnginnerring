72402d8685c832911032953537cf7e44ed206947
Kaoru Hiratsuka <4v4g1n.sakana.k4@gmail.com>
2022-05-11 18:48:34 +0900

    TDMA法を修正してライブラリとして扱う、方程式の導入を別ファイルへ書き直した

---
+++ 20220426/Equation.py
@@ -0,0 +1,69 @@
+#TDMA.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+import TDMA as tdma
+
+def main():
+    """
+    以下pe~dxまで変更する
+    """
+
+    pe = 100.0      #ペクレ数
+    phi2 = 50.0     #チーレ数
+    N = 100         #計算点の数
+    dt = 0.1        #Δt
+    t_max = 20     #計算を止める時刻
+    dx = 1.0 / N    #ΔX
+
+    #3項方程式の係数
+    a = []
+    b = []
+    c = []
+
+    #3項方程式の係数の計算
+    for i in range(N):
+        temp_c = dt / dx**2
+        temp_a = temp_c + pe * dt / dx
+        temp_b = temp_a + temp_c + phi2 * dt + 1
+        a.append(temp_a)
+        b.append(temp_b)
+        c.append(temp_c)
+
+    a[N-1] = a[N-1] + c[N-1]    #i=Nでの境界条件
+
+    #初期条件
+    t = 0.0
+    y = [0.0] * N
+    d = [0.0] * N
+
+    #グラフ横軸指定用
+    x = [0.0] * N
+    c_d = 1.0 / float(N)
+    for i in range(1, N):
+        x[i] = c_d * float(i)
+    print(x)
+    loop_n = 0  #グラフの描画回数指定用
+
+    plt.figure(figsize=(8.5, 5))
+
+    while t < t_max:
+        #3項方程式の右辺の計算
+        for num in range(len(y)):
+            d[num] = y[num]
+
+        d[0] = a[0] * 1.0 + y[0]
+        _y = tdma.TDMA_Solve(a, b, c, d)
+        #print(y)
+        #print(t)
+        if loop_n % 20 == 0:
+            plt.plot(x, _y, label='t={}'.format(round(t, 1)))
+        y = _y
+        t = t + dt
+        loop_n += 1
+
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+        main()
--- 20220426/TDMA.py
+++ 20220426/TDMA.py
@@ -4,65 +4,26 @@ import numpy
 import matplotlib.pyplot as plt
 
 def main():
-    """
-    以下pe~dxまで変更する
-    """
-    
-    pe = 100.0      #ペクレ数
-    phi2 = 50.0     #チーレ数
-    N = 100         #計算点の数
-    dt = 0.1        #Δt
-    t_max = 100     #計算を止める時刻
-    dx = 1.0 / N    #ΔX
-
-    #3項方程式の係数
-    a = []
-    b = []
-    c = []
-
-    #3項方程式の係数の計算
-    for i in range(N+1):
-        temp_c = dt / (dx*dx)
-        temp_a = temp_c + pe * (dt / dx)
-        temp_b = temp_a + temp_c + phi2 * phi2 * dt + 1
-        a.append(temp_a)
-        b.append(temp_b)
-        c.append(temp_c)
-
-    a[N] = a[N] + c[N]    #i=Nでの境界条件
-
-    #初期条件
-    t = 0
-    y = [0.0] * N
-    d = [0.0] * N
-
-    while t < t_max:
-        #3項方程式の右辺の計算
-        d[0] = a[0] * 1.0 + y[0]
-        y = TDMA_Solve(a, b, c, d)
-        #print(y)
-        #print(t)
-        plt.plot(y)
-        plt.show()
-        plt.clf()
-        t = t + dt
+    a=[0.0, 1.0, 2.0, 3.0, 4.0]
+    b=[1.0, 2.0, 3.0, 4.0, 5.0]
+    c=[1.0, 1.0, 1.0, 1.0, 0.0]
+    d=[2.0, 4.0, 6.0, 8.0, 10.0]
+    y=TDMA_Solve(a,b,c,d)
+    print(y)
 
 
 def TDMA_Solve(a, b, c, d):
     p = [c[0]/b[0]]
     q = [d[0]/b[0]]
-
-    for i in range(len(d)):
+    for i in range(1, len(d)):
         p.append(c[i]/(b[i]-a[i]*p[i-1]))
         q.append((d[i]+a[i]*q[i-1])/(b[i]-a[i]*p[i-1]))
 
-    y = [q[len(d)-1]]
-    p.reverse()
-    q.reverse()
-    for i in range(len(d)-2):
-        y.append(p[i]*y[i-1]+q[i])
+    y = [0.0]*len(d)
+    y[len(d)-1] = q[len(d)-1]
+    for i in range(len(d)-2, -1, -1):
+        y[i] = p[i]*y[i+1]+q[i]
 
-    y.reverse()
     return y
 
 if __name__ == '__main__':
