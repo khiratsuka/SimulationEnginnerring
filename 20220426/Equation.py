8e4e693282262e2cb9b825fab5cbe688a1d4177f
Kaoru Hiratsuka <4v4g1n.sakana.k4@gmail.com>
2022-05-24 16:07:29 +0900

    20220426 report

--- 20220426/Equation.py
+++ 20220426/Equation.py
@@ -1,4 +1,4 @@
-#TDMA.py
+#Equation.py
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +9,7 @@ def main():
     以下pe~dxまで変更する
     """
 
-    pe = 100.0      #ペクレ数
+    pe = 200.0      #ペクレ数
     phi2 = 50.0     #チーレ数
     N = 100         #計算点の数
     dt = 0.1        #Δt
@@ -42,7 +42,6 @@ def main():
     c_d = 1.0 / float(N)
     for i in range(1, N):
         x[i] = c_d * float(i)
-    print(x)
     loop_n = 0  #グラフの描画回数指定用
 
     plt.figure(figsize=(8.5, 5))
@@ -54,14 +53,12 @@ def main():
 
         d[0] = a[0] * 1.0 + y[0]
         _y = tdma.TDMA_Solve(a, b, c, d)
-        #print(y)
-        #print(t)
         if loop_n % 20 == 0:
             plt.plot(x, _y, label='t={}'.format(round(t, 1)))
         y = _y
         t = t + dt
         loop_n += 1
-
+    plt.title('ΔX={}, ΔT={}, Pe={}, φ^2={}'.format(dx, dt, pe, phi2))
     plt.legend()
     plt.show()
 
